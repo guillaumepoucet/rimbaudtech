ff9c20d4e6010284e1a61e72912bcecc03737b10
Steven-Blombou <blombou@simplon-charleville.fr>
2020-06-23 09:24:24 +0200

    commit clear code

--- jeu.php
+++ jeu.php
@@ -116,40 +116,9 @@
     <script src="js/modal.js"></script>
     <script src="js/score.js"></script>
     <script src="js/questionnaire.js"></script>
+    <script src="js/jeu.js"></script>
     <script src="js/lancerDe.js"></script>
-    <script>
-
-    $(function () {
-        $('#rep').click(function () {
-            var n = ($('.numero').text() - 1);
-            console.log(n)
-        
-            var a = $("input[type=radio][name=list]:checked").attr("value");
-            var b = questionnaire[n][2];
-            console.log(b)
-            console.log(a)
-            if (a == b) {
-                //alert("Correct");
-                var score = $('.score').text();
-                var s = parseInt(score)
-                newScore = s + 100;
-                $('.score').text(newScore);
-                 modal.style.display = "none";
-      
-            
-            } else {
-                //alert("Incorrect");
-                var score = $('.score').text();
-                var s = parseInt(score)
-                newScore = s + 0;
-                $('.reponse div').remove();
-                $('#rep').remove();
-                $('.reponse').append('<p>Vous etes le maillon faible. Au revoir !</p>');
-                
-            }
-        })
-})
-    </script>
+    
 
 </body>
 
---
+++ js/jeu.js
@@ -0,0 +1,31 @@
+
+    $(function () {
+        $('#rep').click(function () {
+            var n = ($('.numero').text() - 1);
+            console.log(n)
+
+            var a = $("input[type=radio][name=list]:checked").attr("value");
+            var b = questionnaire[n][2];
+            console.log(b)
+            console.log(a)
+            if (a == b) {
+                //alert("Correct");
+                var score = $('.score').text();
+                var s = parseInt(score)
+                newScore = s + 100;
+                $('.score').text(newScore);
+                modal.style.display = "none";
+
+
+            } else {
+                //alert("Incorrect");
+                var score = $('.score').text();
+                var s = parseInt(score)
+                newScore = s + 0;
+                $('.reponse div').remove();
+                $('#rep').remove();
+                $('.reponse').append('<p>Vous etes le maillon faible. Au revoir !</p>');
+
+            }
+        })
+    }) 
