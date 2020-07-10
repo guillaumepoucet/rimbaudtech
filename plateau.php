29be5ec051c2133e31b1cfd760803e4ab251b716
guillaumepoucet <poucet@simplon-charleville.fr>
2020-07-10 10:14:49 +0200

    init.js

--- plateau.php
+++ plateau.php
@@ -188,47 +188,7 @@
 
     <!-- le nombre d'équipes est appelées et le nombre d'instances Team sont crées en conséquence -->
     <script src="js/Team.js"></script>
-
-    <script>
-        objs = [];
-
-
-        // construit un tableau composée de chiffres aléatoires pour la sélection des cases Actions
-        var caseRand = function() {
-            for (y = 1; y <= 18; y++) {
-                do {
-                    nb = Math.floor(Math.random() * 30) + 1;
-                    var nbUsed = caseAction.includes(nb);
-                } while (nbUsed);
-                caseAction.push(nb)
-            }
-            caseAction.sort()
-        }
-
-        var actionCases = function() {
-            for (z = 0; z <= 18; z++) {
-                console.log(caseAction[z]);
-                $('.wrap' + t + ' .case-count').eq(caseAction[z] - 1).addClass('action');
-                $('.wrap' + t + ' .bis .span').eq(caseAction[z] - 1).hide();
-                $('.wrap' + t + ' .bis .svgBubble').eq(caseAction[z] - 1).hide();
-                $('.wrap' + t + ' .bis').eq(caseAction[z] - 1).append("<img class=\"svgAction\" src=\"img/action.svg\" alt=\"\">");
-                $('.wrap' + t + ' .bis').eq(caseAction[z] - 1).append("<img class=\"svgBoom\" src=\"img/boom.svg\" alt=\"\">");
-            }
-        }
-
-        for (t = 1; t <= nbTeam; t++) {
-            name = $('.team' + t + ' .team-name p').text()
-            pawn = $('.team' + t + '-icon').attr('src')
-            objs[t] = new Team(name, pawn)
-
-            caseAction = [];
-
-            // détermination des cases actions
-            caseRand()
-            // transformation des cases en case action
-            actionCases();
-        }
-    </script>
+    <script src="init.js"></script>
 
     <script src="js/questionnaire.js"></script>
     <script src="js/defis.js"></script>
