c93737a894c9251a14d26ff41c2e82b9e6f2f206
guillaumepoucet <poucet@simplon-charleville.fr>
2020-07-15 18:04:52 +0200

    link css renommée

--- newgame.php
+++ newgame.php
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
     <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/plateau.css">
+    <link rel="stylesheet" href="css/style.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
     <style>
